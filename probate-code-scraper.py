f32b41e24f63dc139470a2aaa20cbdfa00b98670
lbove27 <lbove@ucsd.edu>
2026-01-24 17:28:03 -0800

    scraper can scrape from the site but the regex pattern needs to be fixed to cover more cases

--- probate-code-scraper.py
+++ probate-code-scraper.py
@@ -1,22 +1,39 @@
 import requests 
+import re
 from bs4 import BeautifulSoup
 
 def scrape_page(url):
-    # Send HTTP request
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()  # raises error for 4xx/5xx
+    try: 
+        # Send HTTP request
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()  # raises error for 4xx/5xx
 
-    # Parse HTML
-    soup = BeautifulSoup(response.text, "html.parser")
+        # Parse HTML
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    return soup 
+        return soup
+
+    except requests.exceptions.RequestException:
+        # Handles all network-related errors, including 404, timeout, connection errors
+        return False 
 
 if __name__ == "__main__":
-    url = "https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?lawCode=PROB&sectionNum=4."
+
+    code = "1"
+    url = f'https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?lawCode=PROB&sectionNum={code}.'
+
     soup = scrape_page(url)
 
-    # Example: print page title
-    print(soup.title.text)
+    while int(code) < 27100:
+        match = re.search(fr'(?:.*){code}\. \s*(.*)', soup.text, re.DOTALL)
+        if match:
+            code_text = match.group(1).strip()
+            #print("Section number:", code)
+            #print("Text:", code_text)
 
-    with open("output.txt", "a", encoding="utf-8") as f:
-        print(soup.title.text, file=f)
+            with open("output.txt", "a", encoding="utf-8") as f:
+                print(code + ": " + code_text, file=f)
+
+        code = str(int(code) + 1)
+        url = f'https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?lawCode=PROB&sectionNum={code}.'
+        soup = scrape_page(url)
