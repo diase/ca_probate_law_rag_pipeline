3c36bec1026df05b08f3d48be5b9e87db52b7db0
diase <41022702+diase@users.noreply.github.com>
2026-01-27 13:28:17 -0800

    updated files

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,14 +1,12 @@
 import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-#from langchain_text_splitters import CharacterTextSplitter
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-#from langchain_openai import OpenAIEmbeddings
+import re
+#from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
-from webscraper import download_site
 
-#remove this
 import shutil
 if os.path.exists("chroma_db"):
     shutil.rmtree("chroma_db")
@@ -16,29 +14,36 @@ if os.path.exists("chroma_db"):
 load_dotenv()
 
 def main():
-    #Download Wikipedia Pages
-    print("Downloading file")
-    download_site()#
 
-    #1 Load Files
-    print("Loading Files")
+    #1 Load File
+    print("Loading File")
     from functools import partial 
     loader = DirectoryLoader( "docs/", glob="*.txt", loader_cls=partial(TextLoader, encoding="utf-8") )
-    documents = loader.load()
-    print(f"Loaded {len(documents)} documents.")
+    document = loader.load()
+    print(f"Loaded {len(document)} document.")
 
     #2 Chunk Files
-    print("Chunking Files")
-    #splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200,
-                                             separators=["\n\n", "\n", ".", " ", ""])
-    chunks = splitter.split_documents(documents)
+    print("Chunking File")
+    with open("docs/output.txt", "r", encoding="utf-8") as f:
+        text = f.read()
+
+    # Split on "Provision X:" but keep the header
+    parts = re.split(r"(Provision\s+\d+:)", text)
+
+    documents = []
+
+    for i in range(1, len(parts), 2):
+        header = parts[i]
+        body = parts[i+1].strip()
+        full_text = f"{header}\n{body}"
+        documents.append(Document(page_content=full_text))
+    
     print("Chunking complete")
 
     #3 Embed and Store in DB
     print("Embedding and Storing in DB")
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    db = Chroma.from_documents(chunks, embeddings, persist_directory="chroma_db")
+    db = Chroma.from_documents(documents, embeddings, persist_directory="chroma_db")
     
 
     print("Ingestion complete")
