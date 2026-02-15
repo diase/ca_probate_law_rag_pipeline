9e0cca7c4d7432ebc23fa57eb5bba374e1c772f6
diase <41022702+diase@users.noreply.github.com>
2026-02-15 10:47:15 -0800

    updated callable retrievable to do db routing

--- callable_retrieval.py
+++ callable_retrieval.py
@@ -14,10 +14,12 @@ class RetrievalPipeline:
         #1. Embeddings: same as ingestion
         self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-        #2. Load existing Chroma DB
-        self.db = Chroma(persist_directory="chroma_db", embedding_function=self.embeddings)
-        print("Chroma DB loaded")
-        print("Total chunks in DB:", self.db._collection.count())
+        #2. Load existing Chroma DBs
+        self.statute_db = Chroma(persist_directory="statute_db", embedding_function = embeddings)
+        self.self_help_db = Chroma(persist_directory="self_help_db", embedding_function = embeddings)
+        self.rule_db = Chroma(persist_directory="rule_db", embedding_function = embeddings)
+        self.form_db = Chroma(persist_directory="form_db", embedding_function = embeddings)
+        print("Chroma DBs loaded")
 
     def ask_questions(self, question):
         #1. Initialize Gemini LLM
@@ -28,17 +30,34 @@ class RetrievalPipeline:
         
         query = question
         if query.lower() in {"q", "quit", "exit"}:
-            return 
+            return
+        
+        #2. Route to correct db
+        form_keywords = ["form", "file", "submit", "attach", "de-", "de -", "ge-", "ge -", "app-", "app -", "jv-", "jv -"]
+        rule_keywords = ["notice", "deadline", "hearing", "inventory", "petition", "time limit"]
+        statute_keywords = ["inherit", "liable", "duty", "power"]
+        self_help_keywords = ["what is", "when is", "explain", "overview", "basics", "summary", "summarize"]
 
-        #4. Retrieve top k results with scores(Cosine Distance)
-        raw_results = self.db.similarity_search_with_score(query, k=20)
+        if any(w in query.lower() for w in form_keywords):
+            self.db = self.form_db
+        elif any(w in query.lower() for w in rule_keywords):
+            self.db = self.rule_db
+        elif any(w in query.lower() for w in statute_keywords):
+            self.db = self.statute_db
+        elif any(w in query.lower() for w in self_help_keywords):
+            self.db = self.self_help_db
+        else:
+            self.db = self.statute_db
+
+        #3. Retrieve top k results with scores(Cosine Distance)
+        raw_results = self.db.similarity_search_with_score(query, k=10)
         print("\n--- Similarities ---")
         for doc, dist in raw_results:
             similarity = 1 - dist  # Convert distance to similarity
             print(f"distance={dist:.4f}, similarity={similarity:.4f}")
 
         
-        #5 Set cosine similarity threshold
+        #4 Set cosine similarity threshold
         similarities = [1 - dist for _, dist in raw_results]
         #threshold = max(similarities) * 0.2 # keep any document that is at least 20% of the top similarity
         filtered_docs = []
@@ -55,7 +74,7 @@ class RetrievalPipeline:
             if similarity >= threshold:
                 filtered_docs.append((doc, distance))
 
-        #Handle no results
+        #Handle no results(Currently useless as we keep 50%)
         
         if not filtered_docs:
             print(f"\nNo documents found above similarity threshold {threshold}.")
@@ -73,17 +92,13 @@ class RetrievalPipeline:
         #6. Build content for Gemini
         contents = [
             {"role": "user",
-            "parts": [{"text": "You are a helpful assistant answering questions based only on the provided context. Answer clearly and concisely. If the answer is not in the context, say you don't know."}]
-            },
-            {"role": "user",
-            "parts": [
-                {"text": f"Question: {query}"}
-            ]
+             "parts": [{"text": "You are a helpful assistant answering questions based only on the provided context. Answer clearly and concisely. If the answer is not in the context, say you don't know."},
+            {"text": f"Question: {query.lower()}"}]
             }
         ]
         # Add each retrieved document as its own part
         for doc, score in filtered_docs: 
-            contents.append({ "role": "user", "parts": [ {"text": doc.page_content} ] })
+            contents[0]["parts"].append({"text": f"Context: {doc.page_content}"})
 
         #7. Get answer from Gemini
         print("\n--- Answer ---\n")
